0f0257a547dfa5dcaf0d6d835405583a8d1845ee
Yusuf Erdal <yuser604@student.liu.se>
2016-09-21 19:08:43 +0200

    Canvas border, sort of

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@ const UP_KEY = 38;
 const DOWN_KEY = 40;
 const RIGHT_KEY = 39;
 const LEFT_KEY = 37;
+const screenWidth = window.innerWidth-100;
+const screenHeight= window.innerHeight-100;
 var currSpeed = {
     x: 0,
     y: 0
@@ -15,8 +17,8 @@ function startGame() {
 var gameArea = {
     canvas: document.createElement("canvas"),
     start : function() {        
-    	this.canvas.width = window.innerWidth-100;
-    	this.canvas.height = window.innerHeight-100;
+    	this.canvas.width = screenWidth;
+    	this.canvas.height = screenHeight;
         this.context = this.canvas.getContext("2d");
         this.interval = setInterval(updateGameArea, 20);
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
@@ -62,6 +64,27 @@ function component(width, height, color, x, y) {
     	this.angle += this.moveAngle * Math.PI / 180;
         this.position.x += this.speed * Math.sin(this.angle);
         this.position.y -= this.speed * Math.cos(this.angle);
+        this.checkBorder();
+
+    }
+
+    this.checkBorder = function(){
+    	var bottom = this.gamearea.canvas.height - this.height;
+    	var top = this.height;
+    	var leftSide = this.width;
+    	var rightSide = this.gamearea.canvas.width - this.width;
+		if (this.position.y > bottom) {
+			console.log("BOTTOM IS REACEHD");
+	            this.position.y = 2;
+	    }else if(this.position.y < top){
+	    	console.log("TOP REACHED")
+	    	this.position.y = this.gamearea.canvas.height-2;
+	    }else if(this.position.x > rightSide){
+	    	this.position.x = 2;
+	    }
+	    else if(this.position.x < leftSide){
+	    	this.position.x = this.gamearea.canvas.width-2;
+	    }
 
     }
 }
@@ -72,11 +95,13 @@ function updateGameArea() {
     gamePiece.moveAngle = 0;
     if (gameArea.keys && gameArea.keys[LEFT_KEY]) {gamePiece.moveAngle = -4; }
     if (gameArea.keys && gameArea.keys[RIGHT_KEY]) {gamePiece.moveAngle = 4; }
-    if (gameArea.keys && gameArea.keys[UP_KEY]) {gamePiece.speed = -2; }
-    if (gameArea.keys && gameArea.keys[DOWN_KEY]) {gamePiece.speed = 2; }
+    //if (gameArea.keys && gameArea.keys[UP_KEY]) {gamePiece.speed = -2; }
+    //if (gameArea.keys && gameArea.keys[DOWN_KEY]) {gamePiece.speed = 2; }
+    gamePiece.speed = 1.5;
     gamePiece.newPos();
     gamePiece.update();
 }
+
 
 function prevState(direction) {
 
