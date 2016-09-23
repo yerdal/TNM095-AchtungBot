5c31f6fe091aded106694a3ef1828a5371fc9915
einarsandberg <einars92@gmail.com>
2016-09-23 19:23:18 +0200

    added collision detector

--- scripts/Game.js
+++ scripts/Game.js
@@ -3,6 +3,7 @@ const UP_KEY = 38;
 const DOWN_KEY = 40;
 const RIGHT_KEY = 39;
 const LEFT_KEY = 37;
+var i = 0;
 class Game {
 	constructor(width, height) {
 		this.canvas = document.createElement("canvas");
@@ -11,22 +12,25 @@ class Game {
 		this.context = this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 		this.player = new Worm(3, 3, "blue", 10, 120, this);
-		this.enemy = new Worm(3, 3, "red", 10, 110, this);
+		this.enemy = new Worm(3, 3, "red", 150, 110, this);
 		this.addListeners();
+
 	}
+
 	addListeners() {
 		window.addEventListener('keydown', function (e) {
 		    this.keys = (this.keys || []);
 		    this.keys[e.keyCode] = (e.type == "keydown");
-		    console.log(this.keys);
 		}.bind(this));
 		window.addEventListener('keyup', function (e) {
 		    this.keys[e.keyCode] = (e.type == "keydown");
 		}.bind(this));
 	}
+
 	start() {
 		this.interval = setInterval(this.updateGameArea.bind(this), 20);
 	}
+
 	updateGameArea() {
 		this.player.moveAngle = 0;
 		this.enemy.moveAngle = Math.random();
--- scripts/Player.js
+++ scripts/Player.js
@@ -1,47 +1,64 @@
+
 class Player {
 	constructor(width, height, color, x, y, gameArea) {
-		this.gamearea = gameArea;
+		this.gameArea = gameArea;
 		this.width = width;
 		this.height = height;
 		this.speed = 2;
 		this.angle = 0;
 		this.moveAngle = 0;
 		this.color = color;
-		this.ctx = this.gamearea.context;
+		this.ctx = this.gameArea.context;
 		this.position = {
 		    x: x,
 		    y: y
 		};
 	}
+
 	update() {
 		this.ctx.save();
 		this.ctx.translate(this.position.x, this.position.y);
 		this.ctx.rotate(this.angle);
 		this.ctx.fillStyle = this.color;
-		this.ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
+		this.ctx.fillRect(this.width / 2, this.height / 2, this.width, this.height);
 		this.ctx.restore();
+
 	}
+
 	newPos() {
 		this.angle += this.moveAngle * Math.PI / 180;
 	    this.position.x += this.speed * Math.sin(this.angle);
 	    this.position.y -= this.speed * Math.cos(this.angle);
-	    this.checkBorder();
+	    this.checkCollisions();
 	}
 
-	checkBorder() {
-		if(this.position.x+this.width > this.gamearea.canvas.width && this.position.x > this.gamearea.canvas.width){
-			this.ctx.fillRect(this.position.x-this.gamearea.canvas.width, this.position.y, this.width, this.height );
+	checkCollisions() {
+		this.checkWallCollision();
+		this.checkWormCollision();
+	}
+
+	checkWallCollision() {
+		if(this.position.x+this.width > this.gameArea.canvas.width && this.position.x > this.gameArea.canvas.width){
+			this.ctx.fillRect(this.position.x-this.gameArea.canvas.width, this.position.y, this.width, this.height );
 			this.position.x = 0;
 
 		}else if(this.position.x < 0){
-			this.position.x = this.gamearea.canvas.width;
+			this.position.x = this.gameArea.canvas.width;
 		}
 
-		if(this.position.y+this.height > this.gamearea.canvas.height && this.position.y > this.gamearea.canvas.height){
-			this.ctx.fillRect(this.position.x, this.position.y-this.gamearea.canvas.height, this.width, this.height);
+		if(this.position.y+this.height > this.gameArea.canvas.height && this.position.y > this.gameArea.canvas.height){
+			this.ctx.fillRect(this.position.x, this.position.y-this.gameArea.canvas.height, this.width, this.height);
 			this.position.y = 0;
 		}else if(this.position.y < 0){
-			this.position.y = this.gamearea.canvas.height;
+			this.position.y = this.gameArea.canvas.height;
+		}
+	}
+	checkWormCollision() {
+		var pixelColors = this.ctx.getImageData(this.position.x, this.position.y, 1, 1);
+		for (var i = 0; i < pixelColors.data.length; i++) {
+			if (pixelColors.data[i] != 0) {
+				console.log("Hit!");
+			}
 		}
 	}
 
